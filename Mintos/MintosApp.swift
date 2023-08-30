3c6b20bc0d25b42a53fdd6f0eff31f805eda95f0
Mohamed anwar <mohamedanwar18295@gmail.com>
2023-08-30 20:53:18 +0300

    Initial Commit

---
+++ Mintos/MintosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintosApp.swift
+//  Mintos
+//
+//  Created by Mohamed anwar on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MintosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
